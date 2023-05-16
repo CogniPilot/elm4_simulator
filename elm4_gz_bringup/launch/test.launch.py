533217faca5844736f10f87dbd802de4d4cd3a84
James Goppert <james.goppert@gmail.com>
2023-05-16 19:12:25 -0400

    Update test launch file.
    Signed-off-by: James Goppert <james.goppert@gmail.com>

--- elm4_gz_bringup/launch/test.launch.py
+++ elm4_gz_bringup/launch/test.launch.py
@@ -1,11 +1,13 @@
+from os import environ
 from ament_index_python.packages import get_package_share_directory
 
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import PathJoinSubstitution
-
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
 from launch_ros.actions import Node
+from launch.actions import ExecuteProcess
+
 
 ARGUMENTS = [
     DeclareLaunchArgument('world', default_value='elm4map',
@@ -18,30 +20,32 @@ def generate_launch_description():
     synapse_ros = IncludeLaunchDescription(
         PythonLaunchDescriptionSource([PathJoinSubstitution(
             [get_package_share_directory('synapse_ros'), 'launch', 'synapse_ros.launch.py'])]),
-        launch_arguments=[('host', ['127.0.0.1']),
-                          ('port', '4241')],
+        launch_arguments=[('host', ['192.0.2.1']),
+                          ('port', '4242')]
     )
 
     synapse_gz = IncludeLaunchDescription(
         PythonLaunchDescriptionSource([PathJoinSubstitution(
             [get_package_share_directory('synapse_gz'), 'launch', 'synapse_gz.launch.py'])]),
-        launch_arguments=[('host', ['192.0.2.2']),
-                          ('port', '4242')]
+        launch_arguments=[('host', ['127.0.0.1']),
+                          ('port', '4241')],
     )
 
     gz_sim = IncludeLaunchDescription(
         PythonLaunchDescriptionSource([PathJoinSubstitution(
             [get_package_share_directory('ros_gz_sim'), 'launch', 'gz_sim.launch.py'])]),
-        launch_arguments=[
-            ('gz_args', [
-                LaunchConfiguration('world'), '.sdf',
-                ' -v 1',
-                ' -r '])
+        launch_arguments=[('gz_args', [LaunchConfiguration('world'), '.sdf', ' -v 1'])
         ]
     )
+
+    cerebri = ExecuteProcess(
+            cmd=['terminator', '-e', environ.get('CEREBRI_BINARY')],
+            output='screen',
+            shell=True)
 
     return LaunchDescription(ARGUMENTS + [
         synapse_ros,
         synapse_gz,
-        gz_sim
+        gz_sim,
+        cerebri
     ])
